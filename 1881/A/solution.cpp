1dfa9e9d6ae2772b5b23594dbe6b2340ecb23a32
Kaustav Bhalla <kaustavbhalla15@gmail.com>
2026-03-04 06:48:23 +0000

    1881

---
+++ 1881/A/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+
+  string x;
+  cin >> x;
+
+  string s;
+  cin >> s;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
