45ca6433bab03c3e5552462a56c2cba68ef8cd06
Kaustav Bhalla <kaustavbhalla15@gmail.com>
2026-08-05 15:38:31 +0530

    code

--- 1789/A/solution.cpp
+++ 1789/A/solution.cpp
@@ -1,33 +1,40 @@
 #include <bits/stdc++.h>
+#include <numeric>
 using namespace std;
 
-void conquer(vector<int> &a, int low, int mid, int high) {}
-
-void divide(vector<int> &a, int low, int high) {
-  int mid = (high + low) / 2;
-
-  divide(a, low, mid);
-  divide(a, mid + 1, low);
-}
-
 void solve() {
-  int n;
+  long long n;
   cin >> n;
 
-  vector<int> a(n);
+  int flag = 0;
+  vector<long long> a(n);
 
-  for (int i = 0; i < n; i++) {
+  for (long long i = 0; i < n; i++) {
     cin >> a[i];
+  }
+
+  for (long long i = 0; i < n; i++) {
+    for (long long j = i + 1; j < n; j++) {
+      if (gcd(a[i], a[j]) <= 2) {
+        flag = 1;
+      }
+    }
+  }
+
+  if (flag == 0) {
+    cout << "NO\n";
+  } else {
+    cout << "YES\n";
   }
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-
   int t;
   cin >> t;
   while (t--) {
     solve();
   }
+  return 0;
 }
---
+++ 1806/A/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b, c, d;
+
+  cin >> a >> b >> c >> d;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
